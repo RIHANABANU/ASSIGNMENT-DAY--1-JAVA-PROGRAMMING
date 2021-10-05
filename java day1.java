e9736f9d6771217d69bab642e1e0ed27e9c2a16c
Rihanabanu.A <88158973+RIHANABANU@users.noreply.github.com>
2021-10-05 21:54:00 +0530

    DAY 1 ASSIGNMENT JAVA PROGRAMMING SUBMITTED

---
+++ java day1.java
@@ -0,0 +1,30 @@
+class Introduction{
+public static void main(String args[]){
+String fname;
+String lname;
+String fullname;
+int age;
+String bloodgroup;
+String degree;
+double height;
+String grade;
+int cgpa;
+fname="Rihana";
+lname="banu A";
+age=19;
+bloodgroup="o positive";
+degree="BE";
+height=157;
+grade="A";
+cgpa=9;
+System.out.println("My first name is"+" "+fname);
+System.out.println("My last name is" +" " +lname);
+System.out.println("My full name is" +" " +fname+" "+lname);
+System.out.println("My age is"+" "+age);
+System.out.println("My graduation is"+" "+degree);
+System.out.println("My blood group is"+" "+bloodgroup);
+System.out.println("My height is"+" "+height);
+System.out.println("My grade is"+" "+grade);
+System.out.println("My CGPA is"+" "+cgpa);
+}
+}
